42381952fea8fbd93b982893db7dceb4929b5f3c
HorrorOfPartyBeach <e.gilmour33@gmail.com>
2019-10-23 21:09:30 +0100

    Connect to server and display some basic data in browser

--- app/Api.js
+++ app/Api.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 const API_URL = 'http://localhost:3000'
 
 export const getUsers = () => {
-    return axios.get(`${API_URL}/api/search?length=32`)
-    .then(console.log(data, 'data'))
-    .then(({data}) => data.users)
+    return axios.get(`${API_URL}/api/search`)
+    .then(({data}) => {
+        console.log(data, 'data');
+        return data
+    })
 }
--- app/App.js
+++ app/App.js
@@ -1,28 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as api from './Api';
 
 class App extends React.Component{
     state = {
         users: []
     };
 
-    componentDidMount() {
-        fetch('/users')
-          .then(res => res.json())
-          .then(users => this.setState({ users }));
-      };
-
       render() {
+        //console.log(this, 'THIS');
+        const {users} = this.state;
         return (
-          <div className="App">
-            <h1>PlanetRomeo</h1>
-            {this.state.users.map(user =>
-              <div key={user.id}>{user.username}</div>
-            )}
-          </div>
+          <div>
+          <h1>Users</h1>
+          {users.map(user => {
+            console.log(user, 'user1')
+        return <div key={user.id} className="userList">
+          <p>{user.id}</p>
+        <p>{user.name}</p>
+        <p>{user.online_status}</p>
+        <img src={user.picture && user.picture.url} alt="Avatar"></img>
+       </div>
+        })}
+         </div>
         );
       }
+
+      componentDidMount() {
+        console.log('Component mounted...')
+        api.getUsers()
+            .then(data => {
+        console.log(data, 'users')
+          this.setState({
+              users: data.items
+          })
+        })
+    }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+export default App;
