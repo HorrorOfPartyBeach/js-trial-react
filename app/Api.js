b02b9d2cc2ebca01a186f3a8f9c9bce963647caf
HorrorOfPartyBeach <e.gilmour33@gmail.com>
2019-11-02 23:00:42 +0000

    fetch combined data from get responses then map and display it

--- app/Api.js
+++ app/Api.js
@@ -10,49 +10,17 @@ export const getUsers = () => {
     })
 };
 
-// export const getFullUserInfo = async (id) => {
-//     const users = await getUsers();
-//     console.log(users, 'await users');
-//     return axios.get(`${API_URL}/api/profiles?ids=${id}`)
-//     .then(({data}) => {
-//         console.log(data, 'Full data')
-//         return data;
-//     });
-// };
-
-// export const getAllDetails = () => {
-//     return axios.get(`${API_URL}/search`)
-//       .then(((users) => {
-//         const userArr = users.data.items;
-//         console.log(userArr, 'UsArr');
-
-//         const allUsers = userArr.map((eachUser) => {
-//             let id = eachUser.id;
-//             //let profileUrl = `${API_URL}/api/profiles?ids=${id}`
-//             const urls = axios.get(`${API_URL}/profiles?ids=${id}`)
-//             .then((fullUser) => {
-//                 //console.log(fullUser.data[0], 'eachFullUser')
-//                 return fullUser.data[0];
-//             })
-//             return urls
-//         })
-//         //console.log(allUsers, 'array of users');
-//         return allUsers;
-//       }));
-// }
-
 export const getAllDetails = async () => {
     const users = await axios.get(`${API_URL}/search`)
     const userArr = users.data.items;
-        const profiles = [];
+    const profiles = [];
         
         for (const user of userArr) {
-            //console.log(userArr, 'user');
+            // console.log(userArr, 'user');
             const profile = await axios.get(`${API_URL}/profiles?ids=${user.id}`)
-            console.log(profile, 'prof')
+            // console.log(profile, 'prof')
             profiles.push(profile.data[0])
-            console.log(profiles)
+            //console.log(profiles, 'allProfiles');
         }
-        
         return profiles;
 }
--- app/App.js
+++ app/App.js
@@ -8,11 +8,12 @@ import './App.css';
 class App extends React.Component{
     state = {
         users: [],
-        fullInfo: []
+        fullInfo: [],
+        combinedProfiles: []
     };
 
       render() {
-        const {users} = this.state;
+        const {combinedProfiles} = this.state;
         return (
           <div id="app" role="main">
 
@@ -23,14 +24,18 @@ class App extends React.Component{
 <Container id="profileContainer">
   <Row id="profileRow">
 
-          {users.map((user) => {
+          {combinedProfiles.map((user) => {
         return <div key={user.id} className="userProfiles">
 
       <Col sm={3} className="profileCols">
         <img className="profilePic" src={user.picture && user.picture.url || './app/assets/nopic.png'} alt={user.name}></img>
-          <p id="comment">"{user.picture && user.picture.comment || 'No Comment'}"</p>
+          {/* <p id="comment">"{user.picture && user.picture.comment || 'No Comment'}"</p> */}
+          <p id="comment">"{user.headline}"</p>
           <p className="userDetails">Name: {user.name}</p>
-          <p className="userDetails">Online status: {user.online_status}</p>
+          <p className="userDetails">Age: {user.personal.age}</p>
+          {/* <p className="userDetails">Online status: {user.online_status}</p> */}
+          <p className="userDetails">Location: {user.location.city}</p>
+          <p className="userDetails">Distance: {user.location.distance}</p>
           <p className="userDetails">Last Login: {user.last_login}</p>
       </Col>
 
@@ -46,57 +51,76 @@ class App extends React.Component{
 
       componentDidMount() {
         console.log('Component mounted...')
-        //this.fetchAllDetails()
-        this.fetchUsers()
-        this.fetchGetAllDetails()
+        this.fetchUsers();
+        this.fetchAllDetails();
       }
 
-      // fetchAllDetails = () => {
-      //   api.getAllDetails()
-      //   .then(data => {
-      //   console.log(data, 'users')
-      //   this.setState({
-      //     users: data.items
-      //   })
-      //     console.log(data.items, 'Users items');
-      //     return data.items;
-      //   })
-      // .catch(err => {
-      //   console.log(err.message, 'users error');
-      // });
-      // }
-
-      fetchUsers =() => {
-        api.getUsers()
-          .then(data => {
-          //console.log(data, 'users')
-          this.setState({
-            users: data.items
-          })
-            //console.log(data.items, 'Users items');
-            return data.items;
-          })
-        .catch(err => {
-          console.log(err.message, 'users error');
+      fetchUsers = async () => {
+        const basicUsers = await api.getUsers();
+        this.setState({
+          users: basicUsers.items
         });
-      }
-        
-      fetchGetAllDetails = () => {
-        api.getAllDetails()
-        .then(data => {
-          //console.log(data, 'Full users');
-          this.setState({
-              fullInfo: data
-          })
-          console.log(this.state, 'Full users State');
-        })
-        .catch(err => {
-          console.log(err.message, 'Full users error');
-        })
-    };
+      };
 
-    combine
-    
+      fetchAllDetails = async () => {
+        const details = await api.getAllDetails();
+        const users = this.state.users;
+
+        // Concat the two arrays of returned data
+        let merge = [...details, ...users];
+
+        // Reduce the concatenated array to match and combine objects with the same id
+        let allData = merge.reduce(function(acc, curr) {
+          let findIndex = acc.findIndex(function(item) {
+            return item.id === curr.id
+          });
+      
+          if (findIndex === -1) {
+            acc.push(curr)
+          } else {
+            acc[findIndex] = (Object.assign({}, acc[findIndex], curr))
+          }
+      
+          return acc;
+        }, [])
+
+        this.setState({
+          combinedProfiles: allData
+        });
+        console.log(this.state.combinedProfiles, 'profiles');
+      };
+
+
+
+      // fetchUsers = () => {
+      //   api.getUsers()
+      //     .then(data => {
+      //     //console.log(data, 'users')
+      //     this.setState({
+      //       users: data.items
+      //     })
+      //       //console.log(data.items, 'Users items');
+      //       return data.items;
+      //     })
+      //   .catch(err => {
+      //     console.log(err.message, 'users error');
+      //   });
+      // }
+        
+    //   fetchAllDetails = () => {
+    //     api.getAllDetails()
+    //     .then(data => {
+    //       //console.log(data, 'Full users');
+    //       this.setState({
+    //           fullInfo: data
+    //       })
+    //       console.log(data, 'Full users State');
+    //       return data;
+    //     })
+    //     .catch(err => {
+    //       console.log(err.message, 'Full users error');
+    //     })
+    // };    
 }
 
 export default App;
